485105eaddeb92c8958b12cfad954149fe3707be
Elena Starostina <starostina.lena05@gmail.com>
2026-03-19 14:02:44 +0300

    logging

--- quotation-receiver/internal/processor/frame_processor.go
+++ quotation-receiver/internal/processor/frame_processor.go
@@ -3,6 +3,7 @@ package processor
 import (
 	"bytes"
 	"encoding/binary"
+	"encoding/hex"
 	"fmt"
 	"log"
 	"time"
@@ -44,6 +45,9 @@ func (p *FrameProcessor) HandlePayload(payload []byte) error {
 		}
 
 		price := kopecksToRubles(priceKopecks)
+		log.Printf("driver frame raw=%s ticker=%s price_kopecks=%d price_rub=%.2f",
+			hex.EncodeToString(frame), tickerSymbol, priceKopecks, price)
+
 		eventJSON, err := ticker.SerializeEvent(tickerSymbol, price, time.Now())
 		if err != nil {
 			return fmt.Errorf("serialize ticker event: %w", err)
